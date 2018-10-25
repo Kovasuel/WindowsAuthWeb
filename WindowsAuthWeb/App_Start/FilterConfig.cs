51583862e214247c842428a36c50e5d14fb5d3d7
Eddie <eddieguinn@gmail.com>
2018-10-25 10:17:38 -0400

    Add project files.

---
+++ WindowsAuthWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WindowsAuthWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
